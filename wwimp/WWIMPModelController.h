553878d3317c1c9638ccd4213fc06705091ae0a3
Darryl H. Thomas <darrylhthomas@me.com>
2015-11-06 22:02:22 -0800

    Initial Core Data model definition

---
+++ wwimp/WWIMPModelController.h
@@ -0,0 +1,13 @@
+//
+//  WWIMPModelController.h
+//  wwimp
+//
+//  Created by Darryl H. Thomas on 11/5/15.
+//  Copyright © 2015 Darryl H. Thomas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WWIMPModelController : NSObject
+
+@end
---
+++ wwimp/WWIMPModelController.m
@@ -0,0 +1,13 @@
+//
+//  WWIMPModelController.m
+//  wwimp
+//
+//  Created by Darryl H. Thomas on 11/5/15.
+//  Copyright © 2015 Darryl H. Thomas. All rights reserved.
+//
+
+#import "WWIMPModelController.h"
+
+@implementation WWIMPModelController
+
+@end
