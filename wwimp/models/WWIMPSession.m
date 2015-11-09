015680ad2c2a9d2ad3f0ac72b0a71c3b0a9ebad6
Darryl H. Thomas <darrylhthomas@me.com>
2015-11-08 18:34:10 -0800

    Added checks around dependency values for defaults keys.

--- wwimp/models/WWIMPSession.m
+++ wwimp/models/WWIMPSession.m
@@ -10,12 +10,24 @@
 
 - (NSString *)key
 {
-    return [NSString stringWithFormat:@"%@-%@", [self.year stringValue], [self.id stringValue]];
+    NSNumber *year = self.year;
+    NSNumber *id = self.id;
+    NSAssert(year, @"-key called while year is nil");
+    NSAssert(id, @"-key called while id is nil");
+    if (!year || !id) {
+        return nil;
+    }
+    return [NSString stringWithFormat:@"%@-%@", [year stringValue], [id stringValue]];
 }
 
 - (NSString *)lastPlayPositionUserDefaultsKey
 {
-    return [@"last_play_position:" stringByAppendingString:self.key];
+    NSString *key = self.key;
+    NSAssert(key, @"-lastPlayPositionUserDefaultsKey called while key is nil");
+    if (!key) {
+        return nil;
+    }
+    return [@"last_play_position:" stringByAppendingString:key];
 }
 
 @end
